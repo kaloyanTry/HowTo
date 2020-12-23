2484a9328bf1b7a26f9a39a92722c4d9ffbf8a92
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-23 21:57:44 +0200

    Reverse the given number

---
+++ ReverseNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HowToConsole
+{
+    class ReverseNumber
+    {
+        static void Main(string[] args)
+        {
+            //Reverse Number
+
+            int number = int.Parse(Console.ReadLine());
+
+            int reverseNumber = 0;
+
+            while (number != 0)
+            {
+                reverseNumber = (reverseNumber * 10) + (number % 10);
+
+                number /= 10;
+            }
+
+            Console.WriteLine(reverseNumber);
+        }
+    }
+}
