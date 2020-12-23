4f351cc9048e15e797485d35e5c5f6ef1034748f
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-23 21:51:44 +0200

    Facrorial Number Solution

---
+++ FactorialNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HowToConsole
+{
+    class FactorialNumber
+    {
+        static void Main(string[] args)
+        {
+            //Factorial Number
+
+            int n = int.Parse(Console.ReadLine());
+
+            int factorialNumber = 1;
+
+            for (int i = n; i > 1; i--)
+            {
+                factorialNumber *= i;
+                
+            }
+            Console.WriteLine(factorialNumber);
+        }
+    }
+}
