f686573318c12347a9122e7693b89fa57294b57e
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-23 21:40:36 +0200

    First n Fibonacci Numbers

---
+++ FibonacciNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HowToConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fibonacci First N Numbers
+
+            int n = int.Parse(Console.ReadLine());
+
+            int firstNumber = 0;
+            int secondNumber = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(firstNumber);
+
+                int tempNum = firstNumber;
+                firstNumber = secondNumber;
+                secondNumber = tempNum + secondNumber;
+            }
+        }
+    }
+}
