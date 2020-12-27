252e3c08e862598b3b518d0a79ec1e4188058b4a
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-27 08:18:28 +0200

    Reverse strings in an Array
    Reverse the order of the strings in an array

---
+++ ReverseStringsInArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayReverseNumbers
+{
+    class ArrayReverseString
+    {
+        static void Main(string[] args)
+        {
+            string[] stringArray = Console.ReadLine().Split().ToArray();
+
+            string[] reversedArray = new string[stringArray.Length];
+
+            for (int i = 0; i < reversedArray.Length; i++)
+            {
+                reversedArray[i] = stringArray[stringArray.Length - i - 1];
+            }
+
+            Console.WriteLine(string.Join(" ", reversedArray));
+        }
+    }
+}
