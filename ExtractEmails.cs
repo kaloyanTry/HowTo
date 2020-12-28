5b74c52716d65a3ca1c73e19813f200c74f8b8f4
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-28 09:56:44 +0200

    Regex Email Validation
    Regex, Email validation and extraction

---
+++ ExtractEmails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ExractEmails
+{
+    class ExtractEmails
+    {
+        static void Main(string[] args)
+        {
+            string patternEmail = @"(\s[a-z]+[\w.-]+\w)@([a-z]+[-a-z]*?([.][a-z]+)+)\b";
+            Regex regexEmail = new Regex(patternEmail);
+
+            string input = Console.ReadLine();
+
+            Console.WriteLine(string.Join(Environment.NewLine, regexEmail.Matches(input)));
+        }
+    }
+}
