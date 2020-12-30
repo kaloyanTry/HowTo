dae78e0238819cc163c6129e19967901c5730317
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-30 21:29:48 +0200

    Update TriangleWithDolars.cs

--- Draw/TriangleWithDolars.cs
+++ Draw/TriangleWithDolars.cs
@@ -8,7 +8,7 @@ namespace DrawTriangle_
         {
             int n = int.Parse(Console.ReadLine());
 
-            for (int row = 0; row < n; row++)
+            for (int row = 1; row <= n; row++)
             {
                 Console.Write("$");
                 for (int col = 1; col < row; col++)
