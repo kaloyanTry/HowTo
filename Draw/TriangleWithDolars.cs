97406bfeb41a8dff2539a716bed14fd11c9ce295
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-30 21:16:47 +0200

    Draw a triangle with dolar sigh

---
+++ Draw/TriangleWithDolars.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DrawTriangle_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int row = 0; row < n; row++)
+            {
+                Console.Write("$");
+                for (int col = 1; col < row; col++)
+                {
+                    Console.Write(" $");
+                }
+                Console.WriteLine();
+            }    
+        }
+    }
+}
