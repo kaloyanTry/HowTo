a5b1c812f79e45ce7571adc570cf231b555fc7c1
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-30 21:54:53 +0200

    Drawing Romb with stars

---
+++ Draw/RombStars.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace DrawingFigures
+{
+    class RombStars
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+
+            for (int row = 1; row <= n; row++)
+            {
+                for (int col = 1; col <= n - row; col++)
+                {
+                    Console.Write(' ');
+                }
+
+                Console.Write('*');
+                for (int colStar = 1; colStar < row; colStar++)
+                {
+                    Console.Write(" *");
+                }
+                Console.WriteLine();
+            }
+
+            for (int row = 1; row < n; row++)
+            {
+                for (int col = 0; col < row; col++)
+                {
+                    Console.Write(' ');
+                }
+
+                for (int c = 1; c < n - row; c++)
+                {
+                    Console.Write("* ");
+                }
+                Console.Write('*');
+                Console.WriteLine();
+            }
+        }
+    }
+}
