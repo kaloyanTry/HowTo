8ec7634488d3bfe88d1521e4418beba5c19711ae
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-31 11:07:29 +0200

    Drawing a House

---
+++ Draw/House.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DrawingFigures
+{
+    class House
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int stars = 1;
+            if (n % 2 == 0)
+            {
+                stars++;
+            }
+
+            for (int row = 0; row < (n + 1) / 2; row++)
+            {
+                int padding = (n - stars) / 2;
+                Console.WriteLine("{0}{1}{0}", new string('-', padding), new string('*', stars));
+
+                stars += 2;
+            }
+
+            for (int row = 0; row < n / 2; row++)
+            {
+                Console.WriteLine("|{0}|", new string('*', n - 2));
+            }
+        }
+    }
+}
