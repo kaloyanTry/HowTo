6cb238f3f173947ba9672e599620f12bead146b7
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-31 08:46:22 +0200

    Cristmas Tree

---
+++ Draw/CristmasTree.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DrawingFigures
+{
+    class CristmasTree
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+
+            for (int row = 0; row <= n; row++)
+            {
+                Console.WriteLine("{0}{1} | {1}{0}", new string(' ', n - row), new string('*', row));
+            }
+        }
+    }
+}
