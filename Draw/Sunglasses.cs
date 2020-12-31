196f40b854e2fdac22325fd6cf00a0f9fb1bb730
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-31 11:12:02 +0200

    Draw Sunglasses

---
+++ Draw/Sunglasses.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DrawingFigures
+{
+    class CristmasTree
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0}{1}{0}", new string('*', 2 * n), new string(' ', n));
+
+            for (int row = 0; row < n - 2; row++)
+            {
+                if (row == (n - 1) / 2 - 1)
+                {
+                    Console.WriteLine("*{0}*{1}*{0}*", new string('/', 2 * n - 2), new string('|', n));
+                }
+                else
+                {
+                    Console.WriteLine("*{0}*{1}*{0}*", new string('/', 2 * n - 2), new string(' ', n));
+                }
+            }
+
+            Console.WriteLine("{0}{1}{0}", new string('*', 2 * n), new string(' ', n));
+        }
+    }
+}
