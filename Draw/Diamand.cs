9a4093eb47d5a9a1d4da551fc6249b4ea8bf5c6c
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-31 14:26:52 +0200

    Draw Diamand

---
+++ Draw/Diamand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DrawingFigures
+{
+    class House
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int leftRight = (n - 1) / 2;
+
+            for (int row = 1; row <= (n - 1) / 2; row++)
+            {
+                Console.Write(new string('-', leftRight));
+                Console.Write('*');
+                var mid = n - 2 * leftRight - 2;
+
+                if (mid >= 0)
+                {
+                    Console.Write(new string('-', mid));
+                    Console.Write('*');
+                }
+                Console.WriteLine(new string('-', leftRight));
+                leftRight--;
+            }
+
+            for (int row = 0; row <= (n - 1) / 2; row++)
+            {
+                Console.Write(new string('-', leftRight));
+                Console.Write('*');
+                var mid = n - 2 * leftRight - 2;
+
+                if (mid >= 0)
+                {
+                    Console.Write(new string('-', mid));
+                    Console.Write('*');
+                }
+                Console.WriteLine(new string('-', leftRight));
+                leftRight++;
+            }
+        }
+    }
+}
