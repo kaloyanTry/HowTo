3ea0a952fa7c32c58fe6c337c45ec8900e47a454
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-01 09:09:14 +0200

    Draw a square frame

---
+++ Draw/SquareFrame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SquareFrame
+{
+    class SquareFrame
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write('+');
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.Write(" -");
+            }
+            Console.Write(" +");
+            Console.WriteLine();
+
+            for (int row = 0; row < n - 2; row++)
+            {
+                Console.Write('|');
+                for (int i = 0; i < n - 2; i++)
+                {
+                    Console.Write(" -");
+                }
+                Console.Write(" |");
+                Console.WriteLine();
+            }
+
+            Console.Write('+');
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.Write(" -");
+            }
+            Console.Write(" +");
+            Console.WriteLine();
+        }
+    }
+}
