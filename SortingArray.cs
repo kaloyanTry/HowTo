8cc4bfcf0f0fb25cfd3043e95a1ace6597f375a6
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-01-26 15:59:53 +0200

    Sorting Algorithm
    Sorting array

---
+++ SortingArray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace SortingArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < n; i++)
+            {
+                int min = i;
+
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (numbers[j] < numbers[min])
+                    {
+                        min = j;
+                    }
+
+                    int temp = numbers[i];
+                    numbers[i] = numbers[min];
+                    numbers[min] = temp;
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i != n - 1)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+                else
+                {
+                    Console.Write(numbers[i]);
+                }
+            }
+        }
+    }
+}
